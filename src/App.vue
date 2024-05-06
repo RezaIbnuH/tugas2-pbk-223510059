ce6dc85e4b592948a7cd97f7a38e1e56d3679b79
RezaIbnuH <rezaibnu752@gmail.com>
2024-05-06 20:45:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <div class="logo-wrapper">
+      <img alt="Vue logo" class="logo" src="@/assets/Monyet.png" width="220" height="220" />
+      <HelloWorld msg="Reza Ibnu Hanifa" />
+    </div>
+
+    <nav>
+  
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+header {
+ 
+  background-color: transparent;
+  padding: 20px;
+}
+
+.logo-wrapper {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.logo {
+  margin-right: 20px;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #333;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin: 0 10px;
+  transition: background-color 0.3s ease;
+}
+
+nav a:hover {
+  background-color: #ddd;
+}
+
+@media (min-width: 1024px) {
+  .logo-wrapper {
+    margin-bottom: 0;
+  }
+
+  nav {
+    margin-top: 0;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <main class="main">
+    <h1 :style="{ color: textColor }">{{ pageTitle }}</h1>
+    <p :class="{ 'highlight': isHighlighted }">Ketik Sembarang Ajalah</p>
+    <input type="text" v-model="inputText" @input="updateText">
+    <p>{{ inputText }}</p>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const pageTitle = 'Pemrograman Berbasis Komponen';
+const isHighlighted = ref(false);
+const inputText = ref('');
+const textColor = ref('white');
+
+const updateText = (event: Event) => {
+  inputText.value = (event.target as HTMLInputElement).value;
+  textColor.value = inputText.value.length >= 1 ? 'cyan' : 'white';
+};
+</script>
+
+<style>
+body {
+  color: white;
+  background-image: url('/src/assets/Bulan.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main {
+  text-align: center;
+}
+
+h1 {
+  font-size: 48px;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 24px; 
+  line-height: 1.5;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+  border: 2px solid white;
+  border-radius: 5px;
+  margin-bottom: 20px;
+}
+
+.highlight {
+  background-color: white; 
+}
+</style>
